ed5e59f970b176a60ff7bde1896bd9efc027d598
Angel Bran <angel.bran@goes.gob.sv>
2024-08-05 10:52:47 -0600

    refactor:

---
+++ getUpdatesCLI.php
@@ -0,0 +1,67 @@
+#!/usr/bin/env php
+<?php
+
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\Entities\Update;
+
+/**
+ * This file is part of the PHP Telegram Bot example-bot package.
+ * https://github.com/php-telegram-bot/example-bot/
+ *
+ * (c) PHP Telegram Bot Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * This file is used to run the bot with the getUpdates method.
+ */
+
+// Load composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load all configuration options
+/** @var array $config */
+$config = require __DIR__ . '/config.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Telegram($config['bot_api_key'], $config['bot_username']);
+
+    $telegram->useGetUpdatesWithoutDatabase();
+
+    // Configure bot commands
+    $telegram->addCommandsPaths($config['commands']['paths']);
+
+    $telegram->setUpdateFilter(function (Update $update, Telegram $telegram, &$reason = 'Update denied by update_filter') use ($config) {
+        $user_id = $update->getMessage()->getFrom()->getId();
+        if ($user_id == $config['bot_allowed_id']) {
+            return true;
+        }
+
+        $reason = "Invalid user with ID {$user_id}";
+        return false;
+    });
+
+    // Handle telegram getUpdates request
+    $server_response = $telegram->handleGetUpdates();
+
+    if ($server_response->isOk()) {
+        $update_count = count($server_response->getResult());
+        echo date('Y-m-d H:i:s') . ' - Processed ' . $update_count . ' updates';
+    } else {
+        echo date('Y-m-d H:i:s') . ' - Failed to fetch updates' . PHP_EOL;
+        echo $server_response->printError();
+    }
+
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Log telegram errors
+    Longman\TelegramBot\TelegramLog::error($e);
+
+    // Uncomment this to output any errors (ONLY FOR DEVELOPMENT!)
+    // echo $e;
+} catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
+    // Uncomment this to output log initialisation errors (ONLY FOR DEVELOPMENT!)
+    // echo $e;
+}
--- public/index.php
+++ public/index.php
@@ -5,70 +5,83 @@ use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
 use Longman\TelegramBot\Telegram;
 use Longman\TelegramBot\Entities\Update;
+use Longman\TelegramBot\Exception\TelegramException;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 // Load all configuration options
-/** @var array $config */
 $config = require __DIR__ . '/../config.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+// Common function to handle Telegram exceptions
+function handleTelegramException(TelegramException $e, Response $response): Response {
+    $response->getBody()->write($e->getMessage());
+    return $response;
+}
 
+// Function to create and return a Telegram instance
+function createTelegramInstance(array $config): Telegram {
+    return new Telegram($config['bot_api_key'], $config['bot_username']);
+}
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello world!");
     return $response;
 });
 
-# php-telegram-bot
+// php-telegram-bot
 $app->group('/webhook', function(RouteCollectorProxy $group) use ($config) {
-    $group->get('/set', function (Request $request, Response $response, $args) use ($config) {
+    $group->get('/set', function (Request $request, Response $response) use ($config) {
         try {
-            // Create Telegram API object
-            $telegram = new Telegram($config['bot_api_key'], $config['bot_username']);
+            $telegram = createTelegramInstance($config);
 
-            // Set webhook
             $result = $telegram->setWebhook($config['webhook']['url'] . '/webhook/hook');
-            if ($result->isOk()) {
-                $response->getBody()->write($result->getDescription());
-
-                return $response;
-            }
-        } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-            $response->getBody()->write($e->getMessage());
-
-            return $response;
+            $response->getBody()->write($result->getDescription());
+        } catch (TelegramException $e) {
+            return handleTelegramException($e, $response);
         }
+
+        return $response;
     });
 
-    $group->post('/hook', function (Request $request, Response $response, $args) use ($config) {
+    $group->post('/hook', function (Request $request, Response $response) use ($config) {
         try {
-            // Create Telegram API object
-            $telegram = new Telegram($config['bot_api_key'], $config['bot_username']);
+            $telegram = createTelegramInstance($config);
 
-            // Set update filter
-            $telegram->setUpdateFilter(function (Update $update) use ($config) {
+            // Configure bot commands
+            $telegram->addCommandsPaths($config['commands']['paths']);
+
+            $telegram->setUpdateFilter(function (Update $update, Telegram $telegram, &$reason = 'Update denied by update_filter') use ($config) {
                 $user_id = $update->getMessage()->getFrom()->getId();
-                if ($user_id === $config['bot_allowed_id']) {
+                if ($user_id == $config['bot_allowed_id']) {
                     return true;
                 }
 
+                $reason = "Invalid user with ID {$user_id}";
                 return false;
             });
 
-            // Add commands paths containing your custom commands
-            $telegram->addCommandsPaths($config['commands']['paths']);
-
-            // Handle telegram webhook request
             $telegram->handle();
-
             $response->getBody()->write('ok');
-            return $response;
-        } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-            $response->getBody()->write($e->getMessage());
-
-            return $response;
+        } catch (TelegramException $e) {
+            return handleTelegramException($e, $response);
         }
+
+        return $response;
+    });
+
+    $group->get('/unset', function (Request $request, Response $response) use ($config) {
+        try {
+            $telegram = createTelegramInstance($config);
+
+            $result = $telegram->deleteWebhook();
+            $response->getBody()->write($result->getDescription());
+        } catch (TelegramException $e) {
+            return handleTelegramException($e, $response);
+        }
+
+        return $response;
     });
 });
 
